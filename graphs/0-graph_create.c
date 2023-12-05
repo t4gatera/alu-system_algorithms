d38e1638ffcf3b708dcfa68a019d1df1704d24d8
t4gatera <t4gatera@yahoo.com>
2023-12-05 18:45:00 +0200

    Fourth commit on create graph task

--- graphs/0-graph_create.c
+++ graphs/0-graph_create.c
@@ -5,7 +5,7 @@
 typedef struct mygraph{
 int nodes;
 char vertex;
-}
+};
 
 graph_t *create_graph(void){
 graph_t *graph = malloc(sizeof(*graph));
@@ -13,7 +13,8 @@ return graph;
 }
 
 int main(void){
-graph_t *graph = create_graph();
+graph_t *graph = NULL;
+graph = create_graph();
 
 if(!graph){
 fprintf(stderr, "Failed to create graph\n");
