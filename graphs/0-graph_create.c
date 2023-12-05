f33a24c3ea86950bd39e79e57a7ed3ce69a1d9d4
t4gatera <t4gatera@yahoo.com>
2023-12-05 18:22:53 +0200

    First Commit on Create graph

---
+++ graphs/0-graph_create.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"graphs.h"
+
+typedef struct mygraph{
+int nodes;
+char vertex;
+}
+
+graph_t *create_graph(void){
+graph_t *graph = malloc(sizeof(*graph));
+return graph;
+}
+
+int main(void){
+graph_t *graph = create_graph();
+
+if(!grap){
+fprintf(stderr, "Failed to create graph\n");
+return (EXIT_FAILURE);
+}
+
+printf("Successfully created a graph\n");
+return (EXIT_SUCCESS);
+}
